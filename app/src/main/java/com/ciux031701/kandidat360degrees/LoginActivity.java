8fa8aaa7e0f577a0ea284080c8c382d117d1e956
oboking <oscar.boking@gmail.com>
2017-02-13 09:35:39 +0100

    empty project

---
+++ app/src/main/java/com/ciux031701/kandidat360degrees/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.ciux031701.kandidat360degrees;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
