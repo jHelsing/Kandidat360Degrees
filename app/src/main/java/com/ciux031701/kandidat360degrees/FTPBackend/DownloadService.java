a52e1228e381037bcba7c5e228ffa9879ba19b08
Helsing <jonathan.helsing@gmail.com>
2017-02-21 18:09:21 +0100

    Conversion to FTP client
    Started converting HTTP downloading to FTP downloading.

--- app/src/main/java/com/ciux031701/kandidat360degrees/FTPBackend/DownloadService.java
+++ app/src/main/java/com/ciux031701/kandidat360degrees/FTPBackend/DownloadService.java
@@ -4,8 +4,11 @@ import android.app.Activity;
 import android.app.IntentService;
 import android.content.Intent;
 import android.os.Environment;
+import android.util.Log;
+import org.apache.commons.net.ftp.FTPClient;
 
 import java.io.*;
+import java.net.SocketException;
 import java.net.URL;
 
 /**
@@ -17,9 +20,11 @@ public class DownloadService extends IntentService {
 
     private int result = Activity.RESULT_CANCELED;
     private static final String DOMAIN = "saga.olf.sgsnet.se";
-    private static final String PROFILEURL = "profiles";
-    private static final String PANORAMAURL = "panoramas";
-    private static final String PREVIEWURL = "previews";
+    private static final int PORT = 21;
+    private static final String PROFILEURL = "\\profiles\\";
+    private static final String PANORAMAURL = "\\panoramas\\";
+    private static final String PREVIEWURL = "\\previews\\";
+    private static final String FILETYPE = ".jpg";
     public static final String NOTIFICATION = "com.ciux031701.kandidat.360degrees";
 
     public DownloadService() {
@@ -29,29 +34,54 @@ public class DownloadService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         // Create the correct path for the file to be downloaded
-        String fileName = "";
+        String filePath = "";
         switch (intent.getStringExtra("FILETYPE")) {
             case "ICON":
-                fileName = fileName + PREVIEWURL;
+                filePath = filePath + PREVIEWURL;
                 break;
             case "PANORAMA":
-                fileName = fileName + PANORAMAURL;
+                filePath = filePath + PANORAMAURL;
                 break;
             case "PROFILE":
-                fileName = fileName + PROFILEURL;
+                filePath = filePath + PROFILEURL;
+                break;
+            default:
+                this.stopSelf();
                 break;
         }
-        fileName = fileName + "\\" + intent.getStringExtra("FILENAME");
-        File output = new File(Environment.getExternalStorageDirectory(), fileName);
+        String filename = intent.getStringExtra("FILENAME") + FILETYPE;
+        File output = new File(Environment.getExternalStorageDirectory(), filePath + filename);
         if (output.exists()) {
             output.delete();
         }
+
+        // Grab username and password
+        String username = getUsername();
+        String password = getPassword();
+
+        // Start FTP communication
+        FTPClient ftpClient = null;
+
+        try {
+           ftpClient =  new FTPClient();
+           ftpClient.connect(DOMAIN, PORT);
+           Log.d("FTP", "Phone connected to server");
+
+           ftpClient.login(username, password);
+           Log.d("FTP", "Phone logged-in to server");
+
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
         // Open connection to FTP server and download the correct file
         InputStream stream = null;
         FileOutputStream fileOutputStream = null;
         try {
-            URL url = new URL("ftp", DOMAIN, fileName);
+            URL url = new URL("ftp", DOMAIN, filePath);
 
             stream = url.openConnection().getInputStream();
             InputStreamReader reader = new InputStreamReader(stream);
@@ -77,6 +107,22 @@ public class DownloadService extends IntentService {
         }
         publishResults(output.getAbsolutePath(), result, intent.getStringExtra("FILENAME"));
 
+    }
+
+    /**
+     * Loads the username to the FTP server from storage
+     * @return The username to log in with
+     */
+    private String getUsername() {
+        return null;
+    }
+
+    /**
+     * Loads the password to the FTP server from storage
+     * @return The password to log in with
+     */
+    private String getPassword() {
+        return null;
     }
 
     private void publishResults(String outputPath, int result, String fileName) {
