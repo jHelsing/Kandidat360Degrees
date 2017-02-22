4e340902bed28ebcb5430e1cd9e64c1bd900078b
annpeter3 <annpeter@student.chalmers.se>
2017-02-22 18:33:15 +0100

    Added layout to create account-fragment. Started trying to change color on the text depending on number of characters.

--- app/src/main/java/com/ciux031701/kandidat360degrees/CreateAccountFragment.java
+++ app/src/main/java/com/ciux031701/kandidat360degrees/CreateAccountFragment.java
@@ -1,10 +1,14 @@
 package com.ciux031701.kandidat360degrees;
 
 import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import static com.ciux031701.kandidat360degrees.R.id.textView;
@@ -14,14 +18,36 @@ import static com.ciux031701.kandidat360degrees.R.id.textView;
  */
 
 public class CreateAccountFragment extends Fragment {
-    TextView textView;
+    EditText usernameText;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_createaccount, container, false);
 
-        textView = (TextView) root.findViewById(R.id.textView2);
-        textView.setText("Create account here!");
+
+        //To change the color of a text field (here: the usernameField) depending on number of characters
+        //OBS! Is not working yet - does not change the text color
+        usernameText = (EditText) root.findViewById(R.id.usernameField);
+        final TextWatcher txwatcher = new TextWatcher() {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            public void afterTextChanged(Editable s) {
+                usernameText.removeTextChangedListener(this);
+                if(s.length() < 5 && s.length() > 0){
+                    usernameText.setTextColor(Color.RED);
+                } else {
+                    usernameText.setTextColor(Color.BLACK);
+                }
+                usernameText.addTextChangedListener(this);
+            }
+        };
+        usernameText.addTextChangedListener(txwatcher);
+
+
 
         return root;
 
