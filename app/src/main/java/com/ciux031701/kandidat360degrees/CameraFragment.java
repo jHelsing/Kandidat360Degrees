8c5db44184ce1d6eed9dcb049629bd57a8577259
oboking <oscar.boking@gmail.com>
2017-03-11 16:01:07 +0100

    removed fullscreen in post-capture view. Also added support for play-services-panorama

--- app/src/main/java/com/ciux031701/kandidat360degrees/CameraFragment.java
+++ app/src/main/java/com/ciux031701/kandidat360degrees/CameraFragment.java
@@ -12,6 +12,7 @@ import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.PixelFormat;
+import android.graphics.PointF;
 import android.graphics.PorterDuff;
 import android.hardware.Camera;
 import android.hardware.Sensor;
@@ -19,10 +20,15 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.location.GpsSatellite;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.widget.DrawerLayout;
+import android.util.FloatMath;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
@@ -32,6 +38,12 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.panorama.Panorama;
+import com.google.android.gms.panorama.PanoramaApi.PanoramaResult;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
@@ -47,7 +59,9 @@ import java.util.List;
  * Created by boking on 2017-02-17.
  */
 
-public class CameraFragment extends Fragment implements SensorEventListener {
+public class CameraFragment extends Fragment implements SensorEventListener, GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
+
+    private GoogleApiClient mClient;
 
     private TextView holdVerticallyText;
     private ImageButton backButton;
@@ -85,6 +99,25 @@ public class CameraFragment extends Fragment implements SensorEventListener {
     private float orientation[];
     float rField[], iField[];
 
+    //Camera zoom stuff
+    private static final String TAG = "Touch";
+    @SuppressWarnings("unused")
+    private static final float MIN_ZOOM = 1f,MAX_ZOOM = 1f;
+    // These matrices will be used to scale points of the image
+    Matrix matrix = new Matrix();
+    Matrix savedMatrix = new Matrix();
+    // The 3 states (events) which the user is trying to perform
+    static final int NONE = 0;
+    static final int DRAG = 1;
+    static final int ZOOM = 2;
+    int mode = NONE;
+    // these PointF objects are used to record the point(s) the user is touching
+    PointF start = new PointF();
+    PointF mid = new PointF();
+    float oldDist = 1f;
+
+    private float mx,my;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_camera, container, false);
@@ -121,7 +154,6 @@ public class CameraFragment extends Fragment implements SensorEventListener {
         captureButton.setVisibility(View.GONE);
         previewImage.setVisibility(View.GONE);
 
-        //for fullscreenbutton: change to ImageViewActivity
         fullscreenButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -198,7 +230,7 @@ public class CameraFragment extends Fragment implements SensorEventListener {
                     previewImage.setImageBitmap(temp);
                     captureButton.setBackgroundResource(R.drawable.temp_check);
                     backButton.setBackgroundResource(R.drawable.temp_cross);
-                    fullscreenButton.setVisibility(View.VISIBLE);
+                    //fullscreenButton.setVisibility(View.VISIBLE);
                     backButton.setVisibility(View.VISIBLE);
                     previewImage.setVisibility(View.VISIBLE);
                     mSurfaceViewOnTop.setVisibility(View.GONE);
@@ -218,6 +250,21 @@ public class CameraFragment extends Fragment implements SensorEventListener {
 
         //getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         return root;
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mClient = new GoogleApiClient.Builder(getActivity(), this, this)
+                .addApi(Panorama.API)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mClient.connect();
     }
 
     //To save pictures & show the last taken picture:
@@ -419,5 +466,150 @@ public class CameraFragment extends Fragment implements SensorEventListener {
             }
         }
         return bestSize;
+    }
+
+    //Handles manual zooming ontouch of previewimage, commented out since testing other imageviewer.
+    /*@Override
+    public boolean onTouch(View view, MotionEvent event) {
+
+        previewImage.setScaleType(ImageView.ScaleType.MATRIX);
+        float scale;
+
+        dumpEvent(event);
+        // Handle touch events here...
+
+        switch (event.getAction() & MotionEvent.ACTION_MASK)
+        {
+            case MotionEvent.ACTION_DOWN:   // first finger down only
+                savedMatrix.set(matrix);
+                start.set(event.getX(), event.getY());
+                Log.d(TAG, "mode=DRAG"); // write to LogCat
+                mode = DRAG;
+                break;
+
+            case MotionEvent.ACTION_UP: // first finger lifted
+
+            case MotionEvent.ACTION_POINTER_UP: // second finger lifted
+
+                mode = NONE;
+                Log.d(TAG, "mode=NONE");
+                break;
+
+            case MotionEvent.ACTION_POINTER_DOWN: // first and second finger down
+
+                oldDist = spacing(event);
+                Log.d(TAG, "oldDist=" + oldDist);
+                if (oldDist > 5f) {
+                    savedMatrix.set(matrix);
+                    midPoint(mid, event);
+                    mode = ZOOM;
+                    Log.d(TAG, "mode=ZOOM");
+                }
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+
+                if (mode == DRAG)
+                {
+                    matrix.set(savedMatrix);
+                    matrix.postTranslate(event.getX() - start.x, event.getY() - start.y); // create the transformation in the matrix  of points
+                }
+                else if (mode == ZOOM)
+                {
+                    // pinch zooming
+                    float newDist = spacing(event);
+                    Log.d(TAG, "newDist=" + newDist);
+                    if (newDist > 5f)
+                    {
+                        matrix.set(savedMatrix);
+                        scale = newDist / oldDist; // setting the scaling of the
+                        // matrix...if scale > 1 means
+                        // zoom in...if scale < 1 means
+                        // zoom out
+                        matrix.postScale(scale, scale, mid.x, mid.y);
+                    }
+                }
+                break;
+        }
+
+        previewImage.setImageMatrix(matrix); // display the transformation on screen
+
+        return true; // indicate event was handled
+    }*
+
+    private float spacing(MotionEvent event)
+    {
+        float x = event.getX(0) - event.getX(1);
+        float y = event.getY(0) - event.getY(1);
+        return (float)Math.sqrt(x * x + y * y);
+    }
+
+    private void midPoint(PointF point, MotionEvent event)
+    {
+        float x = event.getX(0) + event.getX(1);
+        float y = event.getY(0) + event.getY(1);
+        point.set(x / 2, y / 2);
+    }
+
+    //Show an event in the LogCat view, for debugging
+    private void dumpEvent(MotionEvent event)
+    {
+        String names[] = { "DOWN", "UP", "MOVE", "CANCEL", "OUTSIDE","POINTER_DOWN", "POINTER_UP", "7?", "8?", "9?" };
+        StringBuilder sb = new StringBuilder();
+        int action = event.getAction();
+        int actionCode = action & MotionEvent.ACTION_MASK;
+        sb.append("event ACTION_").append(names[actionCode]);
+
+        if (actionCode == MotionEvent.ACTION_POINTER_DOWN || actionCode == MotionEvent.ACTION_POINTER_UP)
+        {
+            sb.append("(pid ").append(action >> MotionEvent.ACTION_POINTER_ID_SHIFT);
+            sb.append(")");
+        }
+
+        sb.append("[");
+        for (int i = 0; i < event.getPointerCount(); i++)
+        {
+            sb.append("#").append(i);
+            sb.append("(pid ").append(event.getPointerId(i));
+            sb.append(")=").append((int) event.getX(i));
+            sb.append(",").append((int) event.getY(i));
+            if (i + 1 < event.getPointerCount())
+                sb.append(";");
+        }
+
+        sb.append("]");
+        Log.d("Touch Events ---------", sb.toString());
+    }
+    */
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e(TAG, "connection failed: " + connectionResult);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        System.out.println("trying to fix shizzle");
+        Uri uri = Uri.parse("android.resource://" + getActivity().getPackageName() + "/"+ R.drawable.example_panorama);
+        Panorama.PanoramaApi.loadPanoramaInfo(mClient, uri).setResultCallback(
+                new ResultCallback<PanoramaResult>() {
+                    @Override
+                    public void onResult(PanoramaResult result) {
+                        if (result.getStatus().isSuccess()) {
+                            Intent viewerIntent = result.getViewerIntent();
+                            Log.i(TAG, "found viewerIntent: " + viewerIntent);
+                            if (viewerIntent != null) {
+                                startActivity(viewerIntent);
+                            }
+                        } else {
+                            Log.e(TAG, "error: " + result);
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "connection suspended: " + i);
     }
 }
