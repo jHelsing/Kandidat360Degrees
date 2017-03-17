5a061f6068a6c91eabe98973eb1ac23ead6c2f0c
Helsing <jonathan.helsing@gmail.com>
2017-03-17 11:01:29 +0100

    Added a panorama representation of a list item in profile list view

---
+++ app/src/main/java/com/ciux031701/kandidat360degrees/representation/PanoramaProfile.java
@@ -0,0 +1,54 @@
+package com.ciux031701.kandidat360degrees.representation;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ *
+ * Represents a panorama image in the profile
+ *
+ * @author Jonathan
+ * @version 0.1
+ */
+public class PanoramaProfile {
+
+    private int favCount;
+    private String location;
+    private String date;
+    private boolean favorite;
+    private Drawable preview;
+    private int panoramaID;
+
+    public PanoramaProfile(int panoramaID, Drawable preview, boolean favorite, String date, String location, int favCount) {
+        this.panoramaID = panoramaID;
+        this.preview = preview;
+        this.favorite = favorite;
+        this.date = date;
+        this.location = location;
+        this.favCount = favCount;
+    }
+
+    public int getFavCount() {
+        return favCount;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public boolean isFavorited() {
+        return favorited;
+    }
+
+    public Drawable getPreview() {
+        return preview;
+    }
+
+    public int getPanoramaID() {
+        return panoramaID;
+    }
+
+}
