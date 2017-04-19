59f8358ad0fd7d974ed02944fe0cd47974d3df3b
oboking <boking@student.chalmers.se>
2017-04-19 16:11:18 +0200

    login is now executed when the enter key is pressed on the keyboard

--- app/src/main/java/com/ciux031701/kandidat360degrees/LoginFragment.java
+++ app/src/main/java/com/ciux031701/kandidat360degrees/LoginFragment.java
@@ -5,6 +5,7 @@ import android.app.FragmentManager;
 import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -34,7 +35,7 @@ import static android.content.Context.INPUT_METHOD_SERVICE;
  * Created by Anna on 2017-02-23. Modified by Amar on 2017-03-16.
  */
 
-public class LoginFragment extends Fragment {
+public class LoginFragment extends Fragment implements View.OnKeyListener {
     Button loginButton;
     Button createAccountButton;
     TextView title;
@@ -56,52 +57,14 @@ public class LoginFragment extends Fragment {
         errorView.setVisibility(View.INVISIBLE);
 
         loginProgressbar = (ProgressBar) root.findViewById(R.id.loginProgressbar);
+        passwordField.setOnKeyListener(this);
 
         //GUI
         loginButton = (Button)root.findViewById(R.id.loginButton);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                loginProgressbar.setVisibility(View.VISIBLE);
-                Session.setUser(usernameField.getText().toString());
-                String password = passwordField.getText().toString();
-                JReqLogin loginReq = new JReqLogin(Session.getUser(), password);
-                loginReq.setJResultListener(
-                        new JResultListener(){
-                            @Override
-                            public void onHasResult(JSONObject result) {
-                                boolean error;
-                                String sessionId = null;
-                                String message = null;
-                                try{
-                                    error = result.getBoolean("error");
-                                    message = result.getString("message");
-                                    sessionId = result.getString("id");
-                                }
-                                catch(JSONException je){
-                                    error = true;
-                                }
-                                if(!error){
-                                    Session.setId(sessionId);
-                                    Session.save();
-                                    Friends.init();
-                                    FriendRequests.init();
-                                    Intent myIntent = new Intent(getActivity(), MainActivity.class);
-                                    myIntent.putExtra("username", Session.getUser()); //Optional parameters
-                                    startActivity(myIntent);
-
-                                }
-                                else if(message.equals("ERR_USER") | message.equals("ERR_PASS")){
-                                    errorView.setText("Please, make sure that your username or password is correct.");
-                                    errorView.setVisibility(View.VISIBLE);
-                                    usernameField.getBackground().mutate().setColorFilter(getResources().getColor(android.R.color.holo_red_dark) , PorterDuff.Mode.SRC_ATOP);
-                                    passwordField.getBackground().mutate().setColorFilter(getResources().getColor(android.R.color.holo_red_dark) , PorterDuff.Mode.SRC_ATOP);
-                                    loginProgressbar.setVisibility(View.INVISIBLE);
-                                }
-                            }
-                        }
-                );
-                loginReq.sendRequest();
+                executeLogin();
             }
         });
 
@@ -133,6 +96,47 @@ public class LoginFragment extends Fragment {
         return root;
     }
 
+    private void executeLogin(){
+        Session.setUser(usernameField.getText().toString());
+        String password = passwordField.getText().toString();
+        JReqLogin loginReq = new JReqLogin(Session.getUser(), password);
+        loginReq.setJResultListener(
+                new JResultListener(){
+                    @Override
+                    public void onHasResult(JSONObject result) {
+                        boolean error;
+                        String sessionId = null;
+                        String message = null;
+                        try{
+                            error = result.getBoolean("error");
+                            message = result.getString("message");
+                            sessionId = result.getString("id");
+                        }
+                        catch(JSONException je){
+                            error = true;
+                        }
+                        if(!error){
+                            Session.setId(sessionId);
+                            Session.save();
+                            Friends.init();
+                            FriendRequests.init();
+                            Intent myIntent = new Intent(getActivity(), MainActivity.class);
+                            myIntent.putExtra("username", Session.getUser()); //Optional parameters
+                            startActivity(myIntent);
+
+                        }
+                        else if(message.equals("ERR_USER") | message.equals("ERR_PASS")){
+                            errorView.setText("Please, make sure that your username or password is correct.");
+                            errorView.setVisibility(View.VISIBLE);
+                            usernameField.getBackground().mutate().setColorFilter(getResources().getColor(android.R.color.holo_red_dark) , PorterDuff.Mode.SRC_ATOP);
+                            passwordField.getBackground().mutate().setColorFilter(getResources().getColor(android.R.color.holo_red_dark) , PorterDuff.Mode.SRC_ATOP);
+                            loginProgressbar.setVisibility(View.INVISIBLE);
+                        }
+                    }
+                }
+        );
+        loginReq.sendRequest();
+    }
     private void inSession(){
         //Check if a session is saved locally.
         if (Session.load()) {
@@ -171,5 +175,13 @@ public class LoginFragment extends Fragment {
         super.onResume();
         if(loginProgressbar.getVisibility() == View.VISIBLE)
             loginProgressbar.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
+            executeLogin();
+        }
+        return false;
     }
 }
