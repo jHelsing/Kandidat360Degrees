6d5e47dc34e1208b097a6142adf0a475c544c16c
swaziguy123 <mihajlov@chalmers.student.se>
2017-04-22 13:42:15 +0200

    DownloadService now uses retreiveFile instead of retreiveFileStream and a buffer size is set before download. These two changes make it download much faster.

--- app/src/main/java/com/ciux031701/kandidat360degrees/communication/DownloadService.java
+++ app/src/main/java/com/ciux031701/kandidat360degrees/communication/DownloadService.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.IntentService;
 import android.content.Intent;
 import android.util.Log;
+
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 
@@ -55,95 +56,84 @@ public class DownloadService extends IntentService {
         // Make sure that the folder exists where we will store the picture
         File outputDir = new File(getApplicationContext().getFilesDir() + localFilePath);
         if (!outputDir.exists())
-           outputDir.mkdirs();
+            outputDir.mkdirs();
 
         // Create the output file, this is where the downloaded image will be stored
-        File outputFile =  new File(getApplicationContext().getFilesDir() + localFilePath + filename);
+        File outputFile = new File(getApplicationContext().getFilesDir() + localFilePath + filename);
 
         // Checks so that the file already doesn't exist. If it do exist we will publish failure and stop the service.
         // If it does not exist we will create the empty file and if that fails,
         // we will publish failure and stop the service.
         if (!outputFile.exists()) {
-           try {
-              outputFile.createNewFile();
-           } catch (java.io.IOException e) {
-              Log.d("FTP", "File could not be created at location " + outputFile.getPath());
-              publishResults(outputFile.getPath(), Activity.RESULT_CANCELED,
-                      intent.getStringExtra("IMAGEID") + "", type);
-           }
+            try {
+                outputFile.createNewFile();
+            } catch (java.io.IOException e) {
+                Log.d("FTP", "File could not be created at location " + outputFile.getPath());
+                publishResults(outputFile.getPath(), Activity.RESULT_CANCELED,
+                        intent.getStringExtra("IMAGEID") + "", type);
+            }
         } else {
-           publishResults(outputFile.getPath(), Activity.RESULT_OK,
-                   intent.getStringExtra("IMAGEID") + "", type);
+            publishResults(outputFile.getPath(), Activity.RESULT_OK,
+                    intent.getStringExtra("IMAGEID") + "", type);
         }
 
         // Start FTP communication
         FTPClient ftpClient = null;
 
         try {
-           // Start up FTP connection and connect to the FTP server
-           ftpClient =  new FTPClient();
-           ftpClient.connect(FTPInfo.DOMAIN, FTPInfo.PORT);
-           Log.d("FTP", "Phone connected to server: " + ftpClient.getReplyString());
+            // Start up FTP connection and connect to the FTP server
+            ftpClient = new FTPClient();
+            ftpClient.connect(FTPInfo.DOMAIN, FTPInfo.PORT);
+            Log.d("FTP", "Phone connected to server: " + ftpClient.getReplyString());
 
-           // Tries to log onto the server and if it fails it will return
-           // failure, disconnect from server and terminate service
-           if (!ftpClient.login(FTPInfo.getUsername(), FTPInfo.getPassword())) {
-              // Failed the login
-              Log.d("FTP", "FAILED: " + ftpClient.getReplyString());
-              Log.d("FTP", "Returning failed result");
-              Log.d("FTP", "Published results. Closing connection and stopping service.");
-              ftpClient.disconnect();
+            // Tries to log onto the server and if it fails it will return
+            // failure, disconnect from server and terminate service
+            if (!ftpClient.login(FTPInfo.getUsername(), FTPInfo.getPassword())) {
+                // Failed the login
+                Log.d("FTP", "FAILED: " + ftpClient.getReplyString());
+                Log.d("FTP", "Returning failed result");
+                Log.d("FTP", "Published results. Closing connection and stopping service.");
+                ftpClient.disconnect();
 
-              publishResults(outputFile.getPath(), Activity.RESULT_CANCELED,
-                      intent.getStringExtra("IMAGEID") + "", type);
-           } else {
-              // Login successful
-              Log.d("FTP", "Phone logged-in to server: " + ftpClient.getReplyString());
-           }
+                publishResults(outputFile.getPath(), Activity.RESULT_CANCELED,
+                        intent.getStringExtra("IMAGEID") + "", type);
+            } else {
+                // Login successful
+                Log.d("FTP", "Phone logged-in to server: " + ftpClient.getReplyString());
+            }
 
             // Make the server ready for uploading a file to our device
-           ftpClient.enterLocalPassiveMode();
+            ftpClient.enterLocalPassiveMode();
 
-           // Set the correct filetype of the file on the server. For images it is as follows;
-           ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
-           Log.d("FTP", "Correct file type of binary: " + ftpClient.getReplyString());
+            // Set the correct filetype of the file on the server. For images it is as follows;
+            ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+            Log.d("FTP", "Correct file type of binary: " + ftpClient.getReplyString());
 
-           // Set the correct location for the image to be downloaded
-           ftpClient.changeWorkingDirectory(serverFilePath);
-           Log.d("FTP", "Changed Directory: " + ftpClient.getReplyString());
+            // Set the correct location for the image to be downloaded
+            ftpClient.changeWorkingDirectory(serverFilePath);
+            Log.d("FTP", "Changed Directory: " + ftpClient.getReplyString());
+            FileOutputStream outputStream = null;
 
-           // Start downloading the file from the server and write to the local file
-           FileOutputStream outputStream = null;
-           BufferedInputStream input = null;
-           try {
-               // Grab the correct input and output streams
-               outputStream = new FileOutputStream(outputFile.getPath());
-               input = new BufferedInputStream(ftpClient.retrieveFileStream(filename));
+            try {
+                // Start downloading the file from the server and write to the local file
+                outputStream = new FileOutputStream(outputFile);
+                ftpClient.setBufferSize(1024 * 1024);
+                if (ftpClient.retrieveFile(filename, outputStream)) {
+                    // Service results correct. Service has completed it's task and downloaded the correct file
+                    result = Activity.RESULT_OK;
+                    Log.d("FTP", "File downloaded correctly");
+                }
+                else
+                    result = Activity.RESULT_CANCELED;
+            } finally {
+                if (outputStream != null)
+                    outputStream.close();
+            } // Done reading file (try-catch)
 
-               // Start writing the file and and store it, int for int
-               int next = -1;
-               while ((next = input.read()) != -1) {
-                  outputStream.write(next);
-               }
-
-               // make sure to close the input and output streams
-               input.close();
-               outputStream.close();
-
-               // Service results correct. Service has completed it's task and downloaded the correct file
-               result = Activity.RESULT_OK;
-               Log.d("FTP", "File downloaded correctly");
-           } finally {
-               if (outputStream != null)
-                   outputStream.close();
-               if (input != null)
-                  input.close();
-           } // Done reading file (try-catch)
-
-           // Log out and disconnect from server so we do not take up unnecessary connections at server.
-           ftpClient.logout();
-           Log.d("FTP", "Logout: " + ftpClient.getReplyString());
-           ftpClient.disconnect();
+            // Log out and disconnect from server so we do not take up unnecessary connections at server.
+            ftpClient.logout();
+            Log.d("FTP", "Logout: " + ftpClient.getReplyString());
+            ftpClient.disconnect();
 
         } catch (SocketException e) {
             e.printStackTrace();
@@ -156,12 +146,13 @@ public class DownloadService extends IntentService {
 
     }
 
-   /**
-    * Broadcasts the results from the file download.
-    * @param outputPath - The Path where the file is
-    * @param result - The result of the download, -1 if sucessful, 0 if failure
-    * @param fileName - The ID of the image that was downloaded.
-    */
+    /**
+     * Broadcasts the results from the file download.
+     *
+     * @param outputPath - The Path where the file is
+     * @param result     - The result of the download, -1 if sucessful, 0 if failure
+     * @param fileName   - The ID of the image that was downloaded.
+     */
     private void publishResults(String outputPath, int result, String fileName, ImageType type) {
         Intent intent = new Intent(NOTIFICATION + fileName);
         intent.putExtra("IMAGEID", fileName);
